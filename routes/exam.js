20505b0170a5b009adf89133e2d50cdbca3b9d05
No Name <93629090+fnhg1ab0@users.noreply.github.com>
2024-04-01 22:32:12 +0700

    add router exam

--- routes/exam.js
+++ routes/exam.js
@@ -1,6 +1,29 @@
 const express = require('express');
 const {body} = require('express-validator');
 
+const {isAuth} = require('../utils/check-role');
+
+const examController = require('../controllers/exam');
+
 const router = express.Router();
+
+// Get all exams
+// GET /api/v1/exam
+router.get('/', isAuth, examController.getExams);
+
+// Get the data of a specific exam for testing
+// GET /api/v1/exam/:examId
+router.get('/:examId', isAuth, examController.getExamById);
+
+// Submit the exam
+// POST /api/v1/exam/:examId
+router.post('/:examId', isAuth, [
+    body('answers').isArray({min: 3}),
+    body('totalTime').isNumeric().isInt({min: 1}),
+], examController.submitExam);
+
+// Get the result of a specific exam
+// GET /api/v1/exam/result/:examId
+router.get('/result/:examId', isAuth, examController.getResult);
 
 module.exports = router;
